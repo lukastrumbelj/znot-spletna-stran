8706b12dfcb7db50114fc9d19a3d1228558e1104
lukastrumbelj <luka.strumbelj12@gmail.com>
2024-11-30 21:44:50 +0100

    fix

--- resources/views/rezultati.blade.php
+++ resources/views/rezultati.blade.php
@@ -9,7 +9,7 @@
             <section class="info-section margin-top">
                 <h2>Rezultati tekmovanja</h2>                
                 <div class="button-container" style="justify-content:center;">
-                    <a href="" class="btn btn-custom btn-primary" >Poglej rezultate</a>
+                    <a href="https://docs.google.com/spreadsheets/d/1_ptyF0Ml52Veu86LspUajc31aBNp4dKAy1EzNAYc7Gw/edit?gid=605383095#gid=605383095" class="btn btn-custom btn-primary" target="_blank">Poglej rezultate</a>
                 </div>
             </section>
         </div>
